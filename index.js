0e37c892ef186d7c2b209ed0b276348c27bf9c99
Élio Cró <elio@front.no>
2016-11-15 12:55:04 +0000

    Added start button

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ const Hapi = require('hapi');
 const config = require('./config');
 const handler = require('./handler');
 
+const appId = process.env.APP_ID || config.APP_ID;
+const pageId = process.env.PAGE_ID || config.PAGE_ID;
+
 
 const server = new Hapi.Server();
 server.connection({
@@ -13,24 +16,47 @@ server.connection({
   }
 });
 
-
-server.route({
-  method: 'GET',
-  path: '/webhook',
-  config: {
-    handler: handler.verify
+server.register(require('vision'), err => {
+  if (err) {
+    throw err;
   }
-});
 
-server.route({
-  method: 'POST',
-  path: '/webhook',
-  config: {
-    handler: handler.message
-  }
-});
+  server.views({
+    engines: {
+      html: require('handlebars')
+    },
+    path: './www'
+  });
 
+  server.route({
+    method: 'GET',
+    path: '/webhook',
+    config: {
+      handler: handler.verify
+    }
+  });
 
-server.start(() => {
-  console.log('API Server', 'Started on ' + server.info.port);
+  server.route({
+    method: 'POST',
+    path: '/webhook',
+    config: {
+      handler: handler.message
+    }
+  });
+
+  server.route({
+    method: 'GET',
+    path: '/',
+    handler: {
+      view: {
+        template: 'index',
+        context: { appId, pageId }
+      }
+    }
+  });
+
+  server.start(() => {
+    console.log('API Server', 'Started on ' + server.info.port);
+  });
+
 });
