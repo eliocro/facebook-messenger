2f7829b9c57f98ea4e75b265d9e875b21c99312c
Élio Cró <elio@front.no>
2016-11-15 11:12:16 +0000

    First Commit

---
+++ index.js
@@ -0,0 +1,39 @@
+'use strict';
+
+const Hapi = require('hapi');
+const Boom = require('boom');
+const config = require('./config');
+
+
+const server = new Hapi.Server();
+server.connection({
+  port: process.env.PORT || config.PORT,
+  routes: {
+    cors: true
+  }
+});
+
+
+const token = process.env.VERIFY_TOKEN || config.VERIFY_TOKEN;
+
+server.route({
+  method: 'GET',
+  path: '/',
+  config: {
+    handler: (request, reply) => {
+      let qs = request.query;
+
+      if(qs['hub.mode'] === 'subscribe' && qs['hub.verify_token'] === token) {
+        return reply(qs['hub.challenge']);
+      }
+
+      console.error('Failed validation. Make sure the validation tokens match.');
+      reply(Boom.forbidden('Failed validation'));
+    }
+  }
+});
+
+
+server.start(() => {
+  console.log('API Server', 'Started on ' + server.info.port);
+});
